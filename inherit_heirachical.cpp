d8a4617f3e8c4d39c4619ab29c664b4a6330811a
Rudra-IITM <singhrudra5556@gmail.com>
2023-10-07 01:30:30 +0530

    hierarchical and hybrid inheritance

---
+++ inherit_heirachical.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// parent class  
+class A{
+
+};
+
+// first child
+class B:public A{
+
+};
+
+// second child 
+class C:public A{
+
+};
+
+
+// Last for of inherintence is Hybrid inheritence whuch uses all types discussed previously
+int main(){
+    
+
+    return 0;
+}
